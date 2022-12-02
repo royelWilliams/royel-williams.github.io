a5871bf858b4b71fdae4acab744bf05b9ce08f45
royelWilliams <rjnw2014@gmail.com>
2022-12-02 01:26:07 +0000

    a few in the bag

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require(/* Replace this with the name of your lodown! */);
+var _ = require('underbar');
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -16,25 +16,80 @@ var _ = require(/* Replace this with the name of your lodown! */);
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./<YOUR_GITHUB_FOLDER/projects/let-s-get-functional
+ *    npm start --prefix ./royel-williams.github.io/projects/let-s-get-functional
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
-
 var maleCount = function(array) {
-
+    let males=_.filter(array,function(customer,index,array){
+        //return true if the input customer is male 
+        return customer.gender === 'male'; 
+    })
+    return males.length;
+    
 };
 
-var femaleCount;
+var femaleCount = function(array){
+    return _.reduce(array, function(acc, current){
+// number of female customers 
+// if true add one to accumulator 
+if( current.gender === 'female'){
+    return acc += 1;
+}
+else{
+    return acc;
+}
+},0)
+};
 
-var oldestCustomer;
+var oldestCustomer = function(array){
+    let oldest = _.reduce(array, function(acc, current){
+        // determine if the acc is older than the current customer 
+    if(acc.age > current. age){
+        return acc;
+    }
+    else{
+        return current;
+    }
+    },'')
+ // return the name of the oldest customer object 
+ return age.name   
+};
 
-var youngestCustomer;
+var youngestCustomer = function(array){
+    let youngest = _.reduce(array, function(acc, current){
+if(acc.age < current.age){
+    return acc;
+}
+else{
+    return current;
+}
+    },'')
+    return age.name;
+};
 
-var averageBalance;
+var averageBalance = function(array){
+      let average = _.reduce(array, function(acc, current){
+            let bal = parseInt(current.balance.replace(/[^\w\s]/gi, ""),10);
+             acc += bal; 
+             return acc;
+         },0);
+         let sam = (total/(array.length*100));
+         return sam;
+        };
+    
 
-var firstLetterCount;
-
+var firstLetterCount = function(array, letter){
+let first = _.reduce(array, function(acc, current){
+    if(curr.name[0].toLowerCase() === letter.toLowerCase()){
+        acc = acc + 1;
+    }
+     return acc;
+    
+    
+        }, 0);
+    
+    };
 var friendFirstLetterCount;
 
 var friendsCount;
